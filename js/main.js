2c7c5ce92236620466a10d481479bbb56d4222f9
Akhaev <albertakhaev@gmail.com>
2024-04-02 20:08:38 +0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
 
-let a = 2
+//let a = 2
 
-console.log(a)
+//console.log(a)
 
-let b
+//let b
 
-console.log(b)
+//console.log(b)
+
+let today = new Date()
+
+console.log(today)
+console.log("Дата задаётся типом Date")
+
+console.log(Math.PI)
+console.log("Число Пи задаётся типом Math.PI")
+
+const c = 17
+
+console.log("Uncaught TypeError: Assignment to constant variable. При попытке поменять значение const")
+
+let name = prompt("Имя?")    
+let address = prompt("Адрес?")
+let phone = prompt("Номер телефона?")
+
+console.log(`Студент: ${name}, Адрес: ${address}, Номер телефона: ${phone}`)
+
+let ruble = 1
+let dollar = ruble * 92.53
+
+iHave = prompt("Ваш баланс? (В рублях)")
+console.log("Вы можете приобрести: " + iHave / dollar + " долларов")
