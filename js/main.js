bf71df38beb9743846aa2c2f22bdbc73cc565d50
Akhaev <albertakhaev@gmail.com>
2024-04-13 12:54:12 +0300

    initial commit

--- js/main.js
+++ js/main.js
@@ -31,85 +31,118 @@
 // iHave = prompt("Ваш баланс? (В рублях)")
 // console.log("Вы можете приобрести: " + iHave / dollar + " долларов")
 
-const number = 11
+// const number = 11
 
-if(number < 10){
-    console.log('Константа меньше 10')
-}else {
-    console.log('Константа не меньше 10')
-}
+// if(number < 10){
+//     console.log('Константа меньше 10')
+// }else {
+//     console.log('Константа не меньше 10')
+// }
 
-if(4 < 2){
-    console.log('First codeline')
-}else if(5 < 3){
-    console.log('Second codeline')
-}else{
-    console.log('Last codeline')
-}
+// if(4 < 2){
+//     console.log('First codeline')
+// }else if(5 < 3){
+//     console.log('Second codeline')
+// }else{
+//     console.log('Last codeline')
+// }
 
-let loginBD = 'abc'
-let passwordBD = 123
+// let loginBD = 'abc'
+// let passwordBD = 123
 
-console.log('login - abc ; password - 123')
+// console.log('login - abc ; password - 123')
 
-let login = prompt('Make your login')
-let password = prompt('Make your password')
+// let login = prompt('Make your login')
+// let password = prompt('Make your password')
 
-if(loginBD == login){
-    if(passwordBD == password){
-        console.log('Welcome')
+// if(loginBD == login){
+//     if(passwordBD == password){
+//         console.log('Welcome')
+//     }else{
+//         console.log('Password Error')
+//     } 
+// }else{
+//     console.log('Login Error')
+// }
+
+// let key = 'true'
+
+// switch (key) {
+//     case 'false':
+//         console.log('false')
+//         break;
+//     case 'true':
+//         console.log('true')
+//         break;
+
+//     default:
+//         console.log('undefined')
+//         break;
+// }
+
+// let a = 3 < 2 ? 'yes' : 'no'
+
+// console.log(a)
+// console.log('3 Задание ╥')
+
+// let x = 10
+// let y = 7
+
+// if(x > y){
+//     console.log('x больше, чем y')
+// }else{
+//     console.log('x не больше, чем y')
+// }
+
+// let num = prompt('Введите число:')
+
+// if(num%2===0){
+//     console.log(`Число ${num} чётное`)
+// }else{
+//     console.log(`Число ${num} не чётное`)
+// }
+
+// let num1 = prompt('Введите первое число')
+// let num2 = prompt('Введите второе число')
+// let num3 = prompt('Введите третье число')
+
+// if(num1 > num2 && num1 > num3){
+//     console.log(`${num1} наибольшее число`)
+// }else if(num2 > num1 && num2 > num3){
+//     console.log(`${num2} наибольшее число`)
+// }else if(num3 > num1 && num3 > num2){
+//     console.log(`${num3} наибольшее число`)
+// }
+
+function fn(x) {
+    if(x < 10){
+        console.log('константа меньше 10')
     }else{
-        console.log('Password Error')
-    } 
-}else{
-    console.log('Login Error')
+        console.log('константа не меньше 10')
+    }
 }
 
-let key = 'true'
+fn(11)
 
-switch (key) {
-    case 'false':
-        console.log('false')
-        break;
-    case 'true':
-        console.log('true')
-        break;
-
-    default:
-        console.log('undefined')
-        break;
+let HelloWrld = function(){
+    alert('Привет, Мир')
 }
 
-let a = 3 < 2 ? 'yes' : 'no'
+HelloWrld()
 
-console.log(a)
-console.log('3 Задание ╥')
+let js = () => console.log('JavaScript')
 
-let x = 10
-let y = 7
+js()
 
-if(x > y){
-    console.log('x больше, чем y')
-}else{
-    console.log('x не больше, чем y')
+const text = 'lorem text'
+const lorem = 'lorem'
+
+function txt(txxt) {
+    if(text.includes(lorem)){
+        console.log('У вас ошибка в тексте')
+    }else{
+        console.log('Содержимое переменной: ' + txxt)
+    }
 }
 
-let num = prompt('Введите число:')
-
-if(num%2===0){
-    console.log(`Число ${num} чётное`)
-}else{
-    console.log(`Число ${num} не чётное`)
-}
-
-let num1 = prompt('Введите первое число')
-let num2 = prompt('Введите второе число')
-let num3 = prompt('Введите третье число')
-
-if(num1 > num2 && num1 > num3){
-    console.log(`${num1} наибольшее число`)
-}else if(num2 > num1 && num2 > num3){
-    console.log(`${num2} наибольшее число`)
-}else if(num3 > num1 && num3 > num2){
-    console.log(`${num3} наибольшее число`)
-}
+txt(text)
