3e881abb0fcbd4b51ce1d578790c761187797cc4
Akhaev <albertakhaev@gmail.com>
2024-04-17 11:09:04 +0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,85 +1,84 @@
 
-// function fn(x) {
-//     if(x < 10){
-//         console.log('константа меньше 10')
-//     }else{
-//         console.log('константа не меньше 10')
+// let name = 'Albert' // string
+
+// const age = 18 // number
+
+// if(age < 15){
+//     console.log('доступ запрещен')
+// }else{
+//     console.log(`Привет ${name}`)
+// }
+
+// function viewMoney(){
+//     console.log('Вот вам 10$')
+// }
+
+// viewMoney()
+
+// function getMoney(cash){
+//     return (`Вот вам ${cash}$`)
+// }
+
+// function squareNumber(x){
+//     console.log(x * x)
+// }
+
+// squareNumber(9)
+
+// function maxNumber(){
+//     console.log(Math.max.apply(null, arguments))
+// }
+
+// maxNumber(2, 12, 10, 100)
+
+// function discriminant(a, b, c) {
+//     console.log((b*b)-4*a*c)    
+// }
+
+// discriminant(2, 5, 3)
+
+// function math(a, b) {
+//     if(a % 2 == 0 && b % 2 == 0) {
+//         console.log(a * b)
+//     }else if(a % 2 > 0 && b % 2 > 0) {
+//         console.log(a + b)
+//     }else if(a % 2 > 0 && b % 2 == 0) {
+//         console.log(a)
+//     }else {
+//         console.log(b)
 //     }
 // }
 
-// fn(11)
+// math(2, 9)
 
-// let HelloWrld = function(){
-//     alert('Привет, Мир')
-// }
+let man = {
+    name: 'Albert',
+    surname: 'Akhaev',
+    age: 18,
+    tall: 187,
+    login: 'userLogin',
+    password: 'userPassword',
+    balance: 1200000
+}
 
-// HelloWrld()
+man.address = 'Argun'
 
-// let js = () => console.log('JavaScript')
+console.dir(man)
 
-// js()
+let text = 'Password'
 
-// const text = 'lorem text'
-// const lorem = 'lorem'
-
-// function txt(txxt) {
-//     if(text.includes(lorem)){
-//         console.log('У вас ошибка в тексте')
-//     }else{
-//         console.log('Содержимое переменной: ' + txxt)
-//     }
-// }
-
-// txt(text)
-
-let name = 'Albert' // string
-
-const age = 18 // number
-
-if(age < 15){
-    console.log('доступ запрещен')
+if(text == man.password){
+    console.log(`Добро пожаловать, ${man.name} ${man.surname}, Ваш логин: ${man.login}`)
 }else{
-    console.log(`Привет ${name}`)
+    console.log(`Указанный пароль: ${text} – не верен. Попробуйте еще раз.`)
 }
 
-function viewMoney(){
-    console.log('Вот вам 10$')
+let num = 0
+while(num < 3){
+    console.log(`4ucno: ${num}`)
+    num++
 }
 
-viewMoney()
-
-function getMoney(cash){
-    return (`Вот вам ${cash}$`)
+for(let i = 1; i <= 10; i++){
+    console.log(i)
 }
-
-function squareNumber(x){
-    console.log(x * x)
-}
-
-squareNumber(9)
-
-function maxNumber(){
-    console.log(Math.max.apply(null, arguments))
-}
-
-maxNumber(2, 12, 10, 100)
-
-function discriminant(a, b, c) {
-    console.log((b*b)-4*a*c)    
-}
-
-discriminant(2, 5, 3)
-
-function math(a, b) {
-    if(a % 2 == 0 && b % 2 == 0) {
-        console.log(a * b)
-    }else if(a % 2 > 0 && b % 2 > 0) {
-        console.log(a + b)
-    }else if(a % 2 > 0 && b % 2 == 0) {
-        console.log(a)
-    }else {
-        console.log(b)
-    }
-}
-
-math(2, 9)
